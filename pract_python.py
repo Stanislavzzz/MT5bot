56994ceecc4fbc0a56e18f4a6104127e42222050
Ilya-Lugovoy <121395392+Ilya-Lugovoy@users.noreply.github.com>
2023-01-06 14:45:55 +0300

    Replacing from empty to with code

---
+++ pract_python.py
@@ -0,0 +1,7 @@
+array = [2, -32, -1, -1, 41, -5, -7, 6]
+ind = 1
+while ind < len(array):
+    if (array[ind] * array[ind - 1]) > 0:
+        print(array[ind - 1], array[ind])
+        break
+    ind += 1
